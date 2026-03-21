8a97bec5edfecc2ebfdaff045b97c4fc84f0860d
Kajbundit <gadbandidmonta@gmail.com>
2026-03-21 21:33:47 +0700

    add some status

--- frontend/src/pages/restaurant/AcceptOrder.js
+++ frontend/src/pages/restaurant/AcceptOrder.js
@@ -90,6 +90,11 @@ function AcceptOrder () {
     }, [restaurantId]);
 
     const pendingOrdersA = order.filter(item => item.OrderStatus === "accept");
+    const pendingOrdersB = order.filter(item => item.OrderStatus === "success");
+
+    const viewSlip = async () => {
+
+    }
 
     return(
         <div className="min-h-screen w-full flex flex-col items-center bg-gray-100 font-notoSans pb-40">
@@ -131,7 +136,60 @@ function AcceptOrder () {
                                     <span className='pl-2 text-green-700 font-notoSansBold text-2xl'>{item.totalPrice}</span>
                                 </div>
 
-                                
+                                <div className='flex justify-center items-center pt-4'>
+                                    {item.paySlip ? (
+                                        <button
+                                            onClick={() => viewSlip(item.paySlip)}
+                                        >
+
+                                        </button>
+                                    ) : (
+                                        <p>🕓 รอหลักฐานการชำระเงิน</p>
+                                    )}
+                                </div>
+
+                            </div>
+                        )
+                    ))
+                )}
+            </div>
+
+            <div className='pt-12 flex items-center justify-center flex-shrink-0 pb-4'>
+                <p className='text-2xl font-bold'>คำสั่งซื้อที่เสร็จสิ้น</p>
+            </div>
+            
+            <div className='w-80 pt-3'>
+                {pendingOrdersB.length === 0 ? (
+                    <div className='flex justify-center pb-10'>
+                        <p className='pt-14 text-gray-400'>ไม่มีคำสั่งซื้อที่เสร็จสิ้น ณ ตอนนี้ . . .</p>
+                    </div>
+                ) : (
+                    order.map((item, index) => ( item.OrderStatus === "success" && (
+                            <div key={index} className='bg-gray-300 rounded-xl mb-4 shadow-md p-4'>
+                                <h1 className='font-notoSansBold text-blue-700 text-lg mb-2'> 👤 {item.customerName}</h1>
+
+                                {item.items.map((food, idx) => (
+                                    <div key={idx} className='pt-1 border-dashed last:border-0 pb-1'>
+
+                                        <div className='flex justify-between items-center'>
+                                            <p className='pl-4 text-lg'>{food.foodName}</p>
+                                            <span className='font-notoSansBold pr-2 text-lg'>x{food.quantity}</span>
+                                        </div>
+
+                                        {food.note && (
+                                            <p className='text-sm pl-10 text-red-500 italic'>*{food.note}</p>
+                                        )}
+                                    </div>
+                                ))}
+
+                                <div className='flex justify-center pt-4 pb-2'>
+                                    <p className='font-notoSansBold text-sm text-gray-700'>📍 ส่งที่ : {item.address}</p>
+                                </div>
+
+                                <div className='flex justify-center items-center pt-4'>
+                                    <p className='font-bold'>ราคารวม :</p>
+                                    <span className='pl-2 text-green-700 font-notoSansBold text-2xl'>{item.totalPrice}</span>
+                                </div>
 
                             </div>
                         )
